6f516b10a8d458efed34ba0b1fdacfb53fc01d91
Benjamin Cheng <ben@bcheng.me>
2020-11-03 12:36:30 -0700

    add move_test example for testing cubesim.Cube2

---
+++ move_test.py
@@ -0,0 +1,20 @@
+import cubesim
+import numpy as np
+
+c = cubesim.Cube2()
+print(c.state)
+
+move_mappping = {
+        'F': 0, 'F\'': 1,
+        'R': 2, 'R\'': 3,
+        'U': 4, 'U\'': 5,
+        'L': 6, 'L\'': 7,
+        'B': 8, 'B\'': 9,
+        'D': 10, 'D\'': 11,
+}
+while True:
+    move = input('Move:')
+    c.moves[move_mappping[move]]()
+    print(c.state)
+    
+

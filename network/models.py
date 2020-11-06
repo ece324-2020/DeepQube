4dbee1a7ae2344d92f96813a6448cec6709ac47e
Benjamin Cheng <ben@bcheng.me>
2020-11-06 11:47:47 -0700

    implement basic network and replay buffer

---
+++ network/models.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+    """A simple fully connected neural network with customizable
+    input, output, and hidden layer dimensions, as well as activation.
+    """
+    def __init__(self, in_dim, out_dim, layers_dim, activation=F.relu):
+        super(DQN, self).__init__()
+
+        self.activation = activation
+        self.head = nn.Linear(in_dim, layers_dim[0])
+        self.body = nn.ModuleList([
+            nn.Linear(layers_dim[i - 1], layers_dim[i])
+            for i in range(1, len(layers_dim))])
+        self.tail = nn.Linear(layers_dim[-1], out_dim)
+
+    def forward(self, x):
+        # TODO: Should we use dropout?
+        x = self.activation(self.head(x))
+        for layer in self.layers:
+            x = self.activation(layer(x))
+        return self.tail(x)
+

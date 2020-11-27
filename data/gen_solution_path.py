b8be564bc051f8d5c0f25db5a24c179b0dd758b7
Benjamin Cheng <ben@bcheng.me>
2020-11-26 21:21:22 -0500

    data: generate supervised training data

---
+++ data/gen_solution_path.py
@@ -0,0 +1,40 @@
+import functools
+import sys
+
+sys.path.insert(0, '../') # Hack to get cubesim import working
+
+import cubesim
+
+def serialize_state(cube):
+    return ' '.join(map(str, cube.state.reshape(-1).tolist()))
+
+if __name__ == '__main__':
+    with open('./1layer.txt', 'r') as f:
+        lines = f.readlines()
+
+    states = {}
+    for line in lines:
+        scramble = line.strip()
+
+        cube = cubesim.Cube2()
+        cube.load_scramble(scramble)
+
+        for move in reversed(scramble.split(' ')):
+            state = serialize_state(cube)
+            if state in states:
+                break # we've seen how to solve this state before
+
+            # reverse the move itself
+            if move[-1] == "'":
+                move = cube.move_mappping[move[:-1]]
+            else:
+                move = cube.move_mappping[move + "'"]
+
+            states[state] = move
+
+    output = map(lambda item: f'{item[0]}\t{item[1]}\n', states.items())
+
+    with open('./supervised_data.tsv', 'w') as f:
+        f.writelines(output)
+
+

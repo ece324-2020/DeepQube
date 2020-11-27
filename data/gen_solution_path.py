0fd6f138ad2ac65d30e1a3c262546d41537c32db
Benjamin Cheng <ben@bcheng.me>
2020-11-27 17:22:00 -0500

    data: remove duplicates in gen_solution_path

--- data/gen_solution_path.py
+++ data/gen_solution_path.py
@@ -1,36 +1,38 @@
 import functools
-import itertools
 import sys
 
 sys.path.insert(0, '../') # Hack to get cubesim import working
 
 import cubesim
 
-def generate_solution(scramble):
-    cube = cubesim.Cube2()
-    cube.load_scramble(scramble)
-
-    lines = []
-    for move in reversed(scramble.split(' ')):
-        # reverse the move itself
-        if move[-1] == "'":
-            move = cube.move_mappping[move[:-1]]
-        else:
-            move = cube.move_mappping[move + "'"]
-
-        lines.append(f'{serialize_state(cube)}\t{move}')
-    return lines
-
-
 def serialize_state(cube):
     return ' '.join(map(str, cube.state.reshape(-1).tolist()))
 
 if __name__ == '__main__':
-
     with open('./1layer.txt', 'r') as f:
         lines = f.readlines()
 
-    output = itertools.chain(*map(generate_solution, map(lambda line: line.strip(), lines)))
+    states = {}
+    for line in lines:
+        scramble = line.strip()
+
+        cube = cubesim.Cube2()
+        cube.load_scramble(scramble)
+
+        for move in reversed(scramble.split(' ')):
+            state = serialize_state(cube)
+            if state in states:
+                break # we've seen how to solve this state before
+
+            # reverse the move itself
+            if move[-1] == "'":
+                move = cube.move_mappping[move[:-1]]
+            else:
+                move = cube.move_mappping[move + "'"]
+
+            states[state] = move
+
+    output = '\n'.join(map(lambda item: f'{item[0]}\t{item[1]}', states.items()))
 
     with open('./supervised_data.tsv', 'w') as f:
         f.writelines(map(lambda l: f'{l}\n', output))

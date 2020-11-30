11865cad732164531a7b6533d35d68f1bb89ddf2
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-11-29 20:32:08 -0500

    results: add optimal bound to histogram

--- plot_results.py
+++ plot_results.py
@@ -15,7 +15,9 @@ success['DeepQube'] = success['DeepQube'].apply(
     lambda x: 'Solved' if x != 9999 else 'Fail')
 
 plt.figure(1, figsize=(16, 9))
-plot1 = sns.histplot(data=solution_length_filtered)
+plt.axvline(14, 0, color='red', ls='--')
+plot1 = sns.histplot(data=solution_length_filtered, binwidth=1, legend=False)
+plt.legend(('Optimal bound \n (14 moves)', 'Baseline', 'DeepQube'))
 plot1.set(xlabel='Solution move count (quarter turn metric)')
 
 plt.savefig('histogram.png', dpi=300)
@@ -31,7 +33,7 @@ for p in plot2.patches:
     count = success['DeepQube'].value_counts()[i]
 
     plot2.text(p.get_x() + p.get_width()/2.0, h + 50,
-               f"{count} ({count/5000 * 100}%)", ha="center")
+               f'{count} ({count/5000 * 100}%)', ha='center')
     i += 1
 
 plt.savefig('count_plot.png', dpi=300)

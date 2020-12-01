72f63309e84d3b55608fd4a058553ef09704f305
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-12-01 14:27:10 -0500

    demo improvements
    - add error handling
    - add defaults to arg parser
    - handle htm input

--- demo.py
+++ demo.py
@@ -5,15 +5,17 @@ from inference import solve
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='generate the solution to a scramble')
-    parser.add_argument('model1', type=str, help='path to the reinforcement learning model')
-    parser.add_argument('model2', type=str, help='path to the supervised learning model')
     parser.add_argument('scramble', type=str, help='path to file with scrambles to test')
+    parser.add_argument('--model1', type=str, help='path to the reinforcement learning model', default='model1.pt')
+    parser.add_argument('--model2', type=str, help='path to the supervised learning model', default='model2.pt')
 
     args = parser.parse_args()
     model1 = torch.load(args.model1, map_location='cpu')
     model2 = torch.load(args.model2, map_location='cpu')
 
-    is_solved, solution = solve(model1, model2, args.scramble, 40)
+    scramble_qtm = args.scramble.replace('U2', 'U U').replace('R2', 'R R').replace('F2', 'F F')
+
+    is_solved, solution = solve(model1, model2, scramble_qtm, 40)
 
     solution_str = ''
 
@@ -31,4 +33,7 @@ if __name__ == '__main__':
 
     scramble_str = args.scramble.replace("'", "-").replace(" ", "_")
 
-    webbrowser.open(f'https://alg.cubing.net/?setup={scramble_str}&alg={solution_str[:-1]}&puzzle=2x2x2')
+    if is_solved:
+        webbrowser.open(f'https://alg.cubing.net/?setup={scramble_str}&alg={solution_str[:-1]}&puzzle=2x2x2')
+    else:
+        print('Error: DeepQube cannot solve this scramble')

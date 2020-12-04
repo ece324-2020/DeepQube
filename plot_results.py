5b9511744d60f1b91409f44e640fe9ab4b46a664
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-12-04 13:12:10 -0500

    results plot: show distribution of optimal solutions

--- plot_results.py
+++ plot_results.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
@@ -14,11 +15,18 @@ success = pd.read_csv(args.csv)
 success['DeepQube'] = success['DeepQube'].apply(
     lambda x: 'Solved' if x != 9999 else 'Fail')
 
+gods_number_moves = np.arange(4, 15)
+gods_number_range = np.linspace(4, 15, 100)
+gods_number_frequency = 0.01*np.array([0.015, 0.061, 0.24, 0.90, 3.11, 9.81, 25.33, 36.77, 21.3, 2.46, 0.0075])
+z = np.polyfit(gods_number_moves, 5000*gods_number_frequency, 2)
+p = np.poly1d(z)
+
 plt.figure(1, figsize=(16, 9))
-plt.axvline(14, 0, color='red', ls='--')
+plt.plot(gods_number_range, p(gods_number_range), 'r--')
 plot1 = sns.histplot(data=solution_length_filtered, binwidth=1, legend=False)
-plt.legend(('Optimal bound \n (14 moves)', 'Baseline', 'DeepQube'))
+plt.legend(('Probabilistic distribution \n of optimal solutions', 'Baseline', 'DeepQube'))
 plot1.set(xlabel='Solution move count (quarter turn metric)')
+plt.ylim(bottom=0)
 
 plt.savefig('histogram.png', dpi=300)
 
